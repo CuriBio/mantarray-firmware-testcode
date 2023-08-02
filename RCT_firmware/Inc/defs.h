757845ae29b48b50f74f0325e9a29bad97a196d0
Vassily Petrov <vassily.petrov@curibio.com>
2023-08-02 12:38:58 -0700

    established i2c communication, working on actual hardware

--- RCT_firmware/Inc/defs.h
+++ RCT_firmware/Inc/defs.h
@@ -1,17 +1,17 @@
 #ifndef DEFS_H
 #define DEFS_H
 
-#define I2C_SPEED 100E3
+#define I2C_SPEED 50E3
 #define I2C_SDA_PIN 0
 #define I2C_SCL_PIN 1
 
-#define OUTPUT_EXP_ADDR_0 0x46
-#define OUTPUT_EXP_ADDR_1 0x40
-#define INPUT_EXP_ADDR_0 0x42
-#define INPUT_EXP_ADDR_1 0x44
+#define OUTPUT_EXP_ADDR_0 0x46 >>1
+#define OUTPUT_EXP_ADDR_1 0x40 >>1
+#define INPUT_EXP_ADDR_0 0x42 >>1
+#define INPUT_EXP_ADDR_1 0x44 >>1
 
-#define INPUT_BASE_ADDR 0x00
-#define OUTPUT_BASE_ADDR 0x05
+#define INPUT_BASE_ADDR 0x00 
+#define CONFIG_BASE_ADDR 0x0F 
 
 #define STATUS_LED_GREEN 13
 #define STATUS_LED_RED   14
--- RCT_firmware/main.c
+++ RCT_firmware/main.c
@@ -10,11 +10,21 @@
 #include "defs.h"
 
 void comms_error(){
-    gpio_put(STATUS_LED_GREEN, false);
-    gpio_put(STATUS_LED_RED, true);
-    while(1);
+    while(1){
+        gpio_put(STATUS_LED_GREEN, true);
+        gpio_put(STATUS_LED_RED, false);
+        sleep_ms(150);
+        gpio_put(STATUS_LED_RED, true);
+        gpio_put(STATUS_LED_GREEN, false);
+        sleep_ms(150);
+    }
 }
 
+
+uint8_t pin_remap(uint8_t pin_input){
+    if( pin_input <= 24 ) return pin_input*2+1;
+    return (50-pin_input)*2;
+}
 
 int main() {
     stdio_init_all();
@@ -33,26 +43,38 @@ int main() {
     gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
 
+    for(int i=0; i<4; i++){
+        gpio_put(STATUS_LED_GREEN, true);
+        gpio_put(STATUS_LED_RED, false);
+        sleep_ms(100);
+        gpio_put(STATUS_LED_RED, true);
+        gpio_put(STATUS_LED_GREEN, false);
+        sleep_ms(100);
+    }
+    gpio_put(STATUS_LED_RED, false);
+    gpio_put(STATUS_LED_GREEN, false);
+    //while(!stdio_usb_connected());
 
     // validate that all expanders are communicating
+    uint8_t found_devices = 0;
     uint8_t devices[] = {INPUT_EXP_ADDR_0, INPUT_EXP_ADDR_1, OUTPUT_EXP_ADDR_0, OUTPUT_EXP_ADDR_1};
     for(int device=0; device<sizeof(devices); device++){
-        uint8_t dummy;
-        int bread = i2c_read_timeout_us(i2c0, device, &dummy, 1, false, 1000);
-        if( bread != 1 ){
-            // Device not found. No need to do anything else.
-            comms_error();
-        }
+        uint8_t dummy = 0 ;
+        int bread = i2c_write_timeout_us(i2c0, devices[device], &dummy, 1, false, 1000);
+        printf("Device 0x%x: %s\n", devices[device] << 1, bread == 1 ? "PASS" : "FAIL");
+        if(bread == 1) found_devices++;
+    }
+
+    if(found_devices != 4){
+        comms_error();
     }
     
-    sleep_ms(500);
     printf("Program initialized.\n");
 
     while (true) {
 
-        bool cable_good = true;
-
         printf("========================\n");
+        uint8_t results[51];
         // set one output high and test that all but the corresponding input is low
         for(int i=0; i<50; i++){
 
@@ -67,52 +89,84 @@ int main() {
 
 
             // set active output bits
-            byte_buffer[0] = OUTPUT_BASE_ADDR;
-            memcpy(&byte_buffer[1], &pins, 4);
-            n_trans = i2c_write_timeout_us(i2c0, output_addr_act, byte_buffer, 5, false, 1000);
+            byte_buffer[0] = CONFIG_BASE_ADDR;
+            uint32_t pins_inv = ~pins;
+            memcpy(&byte_buffer[1], &pins_inv, 4);
+            n_trans = i2c_write_timeout_us(i2c0, output_addr_act, byte_buffer, 5, false, 2000);
             if( n_trans != 5 ) comms_error();
 
             // set unused bits
-            byte_buffer[0] = OUTPUT_BASE_ADDR;
-            memset(&byte_buffer[1], 0, 4);
-            n_trans = i2c_write_timeout_us(i2c0, output_addr_alt, byte_buffer, 5, false, 1000);
+            byte_buffer[0] = CONFIG_BASE_ADDR;
+            memset(&byte_buffer[1], 0xFF, 4);
+            n_trans = i2c_write_timeout_us(i2c0, output_addr_alt, byte_buffer, 5, false, 2000);
             if( n_trans != 5 ) comms_error();
 
             // read active input bits
             byte_buffer[0] = INPUT_BASE_ADDR;
-            n_trans = i2c_write_timeout_us(i2c0, input_addr_act, byte_buffer, 1, true, 1000);
+            n_trans = i2c_write_timeout_us(i2c0, input_addr_act, byte_buffer, 1, true, 2000);
             if( n_trans != 1 ) comms_error();
             memset(byte_buffer, 0, 4);
-            n_trans = i2c_read_timeout_us(i2c0, input_addr_act, byte_buffer, 4, false, 1000);
+            n_trans = i2c_read_timeout_us(i2c0, input_addr_act, byte_buffer, 4, false, 2000);
             if( n_trans != 4 ) comms_error();
-            uint32_t pins_read_act = *((uint32_t*)byte_buffer) & ~(0b1111111 << 24); // top 7 IO pins are not used and need to be masked off
+            uint32_t pins_read_act = *((uint32_t*)byte_buffer) & ~(0b1111111 << 25); // top 7 IO pins are not used and need to be masked off
 
             // read alt bits
             byte_buffer[0] = INPUT_BASE_ADDR;
-            n_trans = i2c_write_timeout_us(i2c0, input_addr_alt, byte_buffer, 1, true, 1000);
+            n_trans = i2c_write_timeout_us(i2c0, input_addr_alt, byte_buffer, 1, true, 2000);
             if( n_trans != 1 ) comms_error();
             memset(byte_buffer, 0, 4);
-            n_trans = i2c_read_timeout_us(i2c0, input_addr_alt, byte_buffer, 4, false, 1000);
+            n_trans = i2c_read_timeout_us(i2c0, input_addr_alt, byte_buffer, 4, false, 2000);
             if( n_trans != 4 ) comms_error();
-            uint32_t pins_read_alt = *((uint32_t*)byte_buffer) & ~(0b1111111 << 24); // top 7 IO pins are not used and need to be masked off
+            uint32_t pins_read_alt = *((uint32_t*)byte_buffer) & ~(0b1111111 << 25); // top 7 IO pins are not used and need to be masked off
 
             if( pins_read_act != pins || pins_read_alt != 0){
-                cable_good = false;
-                char* result;
                 switch( __builtin_popcount(pins_read_act) + __builtin_popcount(pins_read_alt)){
                     case 0:
-                        result = "OPEN";
+                        // OPEN
+                        results[pin_remap(i)] = 1;
                         break;
                     case 1:
-                        result = "CROSSED";
+                        // CROSSED
+                        results[pin_remap(i)] = 2;
                         break;
                     default:
-                        result = "SHORTED";
+                        // SHORTED
+                        results[pin_remap(i)] = 3;
                 }
-                printf("PIN %d: %s.\n", i, result);
+            }
+            else{
+                results[pin_remap(i)] = 0;
             }
         }
         
+        bool cable_26pin = true;
+        bool cable_good = true;
+        for(int i=1; i<=50; i++){
+            if( i <= 26 && results[i] != 0) cable_26pin = false;
+            if( i > 26 && results[i] != 1) cable_26pin = false;
+        }
+        for(int i=1; i<=( cable_26pin ? 26 : 50); i++){
+            char* result = "";
+            switch(results[i]){
+                case 1:
+                    result = "OPEN";
+                    break;
+                case 2:
+                    result = "CROSSED";
+                    break;
+                case 3:
+                    result = "SHORTED";
+                    break;
+            }
+            if(results[i] > 0){
+                printf("PIN %02d: %s\n", i, result);
+                cable_good = false;
+            }
+        }
+
+        printf("\nCABLE TYPE: %s\n", cable_26pin ? "26 PIN" : "50 PIN");
+        printf("RESULT: %s\n", cable_good ? "PASS" : "FAIL");
+
         if(stdio_usb_connected()){
             if( cable_good ){
                 gpio_put(STATUS_LED_RED, false);
@@ -121,21 +175,29 @@ int main() {
                 gpio_put(STATUS_LED_RED, true);
                 gpio_put(STATUS_LED_GREEN, false);
             }
-            sleep_ms(500);
+
+            sleep_ms(700);
             gpio_put(STATUS_LED_RED, false);
             gpio_put(STATUS_LED_GREEN, false);  
 
+            while(getchar_timeout_us(100) != PICO_ERROR_TIMEOUT);
+            printf("PRESS ENTER TO TEST AGAIN...\n");
+            while(getchar_timeout_us(100) == PICO_ERROR_TIMEOUT && stdio_usb_connected());
+
+
+            
+
         }else{
-            sleep_ms(250); // idk.
+            sleep_ms(120); // idk.
             if( cable_good ){
                 gpio_put(STATUS_LED_RED, false);
-                gpio_put(STATUS_LED_GREEN, gpio_get(STATUS_LED_GREEN));
+                gpio_put(STATUS_LED_GREEN, !gpio_get(STATUS_LED_GREEN));
             }else{
                 gpio_put(STATUS_LED_GREEN, false);
-                gpio_put(STATUS_LED_RED, gpio_get(STATUS_LED_RED));
+                gpio_put(STATUS_LED_RED, !gpio_get(STATUS_LED_RED));
             }
         }
 
-        printf("RESULT: %s\n", cable_good ? "PASS" : "FAIL");
+        
     }
 }
